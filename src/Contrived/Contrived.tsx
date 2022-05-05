0c629abe5c9367b7e6f6902e0c6a173f3a8f51b8
Chris Wiseman <chris@jackhabbit.com>
2022-05-05 15:39:46 -0300

    Initial commit

---
+++ src/Contrived/Contrived.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '@aws-amplify/ui-react/styles.css';
+
+const Contrived = () => <div>Component</div>
+
+export default Contrived;
---
+++ src/Contrived/index.ts
@@ -0,0 +1,1 @@
+export { default as Contrived } from './Contrived';
